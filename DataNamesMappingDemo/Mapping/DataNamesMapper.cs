cfbc80dc01f855136a6484f64d6e94df5e722d14
exceptionnotfound <mpjones428@gmail.com>
2017-02-08 08:59:48 -0700

    Created mapper interface and class

---
+++ DataNamesMappingDemo/Mapping/DataNamesMapper.cs
@@ -0,0 +1,53 @@
+﻿using DataNamesMappingDemo.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataNamesMappingDemo.Mapping
+{
+    public class DataNamesMapper<TEntity> : IDataNamesMapper<TEntity> where TEntity : class, new()
+    {
+        public TEntity Map(DataRow row)
+        {
+            TEntity entity = new TEntity();
+            return Map(row, entity);
+        }
+
+        public TEntity Map(DataRow row, TEntity entity)
+        {
+            var columnNames = row.Table.Columns.Cast<DataColumn>().Select(x => x.ColumnName).ToList();
+            var properties = (typeof(TEntity)).GetProperties()
+                                              .Where(x => x.GetCustomAttributes(typeof(DataNamesAttribute), true).Any())
+                                              .ToList();
+            foreach (var prop in properties)
+            {
+                PropertyMapHelper.Map(typeof(TEntity), row, prop, entity);
+            }
+
+            return entity;
+        }
+
+        public List<TEntity> Map(DataTable table)
+        {
+            List<TEntity> entities = new List<TEntity>();
+            var columnNames = table.Columns.Cast<DataColumn>().Select(x => x.ColumnName).ToList();
+            var properties = (typeof(TEntity)).GetProperties()
+                                              .Where(x => x.GetCustomAttributes(typeof(DataNamesAttribute), true).Any())
+                                              .ToList();
+            foreach (DataRow row in table.Rows)
+            {
+                TEntity entity = new TEntity();
+                foreach (var prop in properties)
+                {
+                    PropertyMapHelper.Map(typeof(TEntity), row, prop, entity);
+                }
+                entities.Add(entity);
+            }
+
+            return entities;
+        }
+    }
+}
---
+++ DataNamesMappingDemo/Mapping/IDataNamesMapper.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataNamesMappingDemo.Mapping
+{
+    public interface IDataNamesMapper<TEntity> where TEntity : class, new()
+    {
+        TEntity Map(DataRow row);
+        TEntity Map(DataRow row, TEntity entity);
+        List<TEntity> Map(DataTable table);
+    }
+}
